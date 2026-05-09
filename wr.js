367dd334941cc189858796e7024d9f788ef3c70f
oAndersu <andersoncarelly924@gmail.com>
2026-05-09 11:04:42 -0300

    Integração com API do clima

--- wr.js
+++ wr.js
@@ -1,4 +1,29 @@
 // Variaveis Inciais
 const cidade = document.getElementById('Cidade')
-const Botão = document.getElementById('Buscar')
-const results = document.getElementById('Resultados')
+const button = document.getElementById('Botaobs')
+const result = document.getElementById('Resultado')
+
+//Botão e integração com API
+ button.addEventListener('click', function (){
+ if(cidade.value === ''){
+     return
+ }
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cidade.value+ '&appid=7fd9c0c4a43abe57fa0437f4ae44714f&units=metric&lang=pt')
+    .then(function (response) {
+     return response.json()
+    })
+    //Data da api
+    .then(function (data) {
+        if (data.cod ==='404'){
+            result.innerHTML = '<p>Cidade Não encontrada!</p>'
+            return
+        }
+     result.innerHTML = `
+      <h1>${data.name}</h1> 
+      <p><strong>Temperatura:</strong> ${data.main.temp}°C</p>
+      <p><strong>Clima:</strong> ${data.weather[0].description}</p>
+      <p><strong>Umidade:</strong> ${data.main.humidity}%</p >`
+    })
+
+
+ })
